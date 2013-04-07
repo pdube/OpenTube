309fdebd4a6ac579ba49c2f4f79e550a0be699ac
Patrick Dube <patrickdube91@gmail.com>
2013-04-07 16:04:52 -0400

    Preliminary search and layout - just testing

---
+++ search.js
@@ -0,0 +1,48 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function loadVideo(playerUrl, autoplay) {
+    swfobject.embedSWF(
+            playerUrl + '&rel=1&border=0&fs=1&autoplay=' +
+            (autoplay ? 1 : 0), 'player', '1100', '800', '9.0.0', false,
+            false, {allowfullscreen: 'true'});
+}
+
+function showMyVideos2(data) {
+    var feed = data.feed;
+    var entries = feed.entry || [];
+    var html = ['<ul class="videos">'];
+    for (var i = 0; i < entries.length; i++) {
+        var entry = entries[i];
+        var title = entry.title.$t.substr(0, 20);
+        var thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
+        var playerUrl = entries[i].media$group.media$content[0].url;
+        html.push('<li onclick="loadVideo(\'', playerUrl, '\', true)">',
+                '<span class="titlec">', title, '...</span><br /><img src="',
+                thumbnailUrl, '" width="130" height="97"/>', '</span></li>');
+    }
+    html.push('</ul><br style="clear: left;"/>');
+    document.getElementById('videos2').innerHTML = html.join('');
+    if (entries.length > 0) {
+        loadVideo(entries[0].media$group.media$content[0].url, false);
+    }
+}
+
+function search() {
+    var t = document.getElementById("test");
+    t.parentNode.removeChild(t);
+    var element = document.createElement("script");
+    element.type = "text/javascript";
+    element.id = "test";
+    element.src= 'http://gdata.youtube.com/feeds/api/videos?alt=json-in-script&callback=showMyVideos2&max-results=10&q='+encodeURIComponent(document.getElementById('s').value);
+    document.body.appendChild(element); 
+}
+function enter(e){
+        if(e.keyCode === 13){
+            search();
+            return true;
+        }
+        return false;
+}
+
